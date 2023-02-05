393bb369317bd94bdc9d4e4de10df30002f4489f
jbisits <jbisits@gmail.com>
2023-02-06 09:59:21 +1100

    Have a direct plot in makie from raster!

--- scripts/makierasterplot.jl
+++ scripts/makierasterplot.jl
@@ -1,19 +1,16 @@
 using CairoMakie
 using .VerticalProfileStability
 
-# Trying to write `convert_arguments` for a `Raster`
+# Trying to write `convert_arguments` for a `Raster`, this works!!
 
-function Makie.convert_arguments(rs::Raster)
+function Makie.convert_arguments(P::SurfaceLike, rs::Raster)
 
     lon, lat = collect(lookup(rs, X)), collect(lookup(rs, Y))
     plot_var = Matrix(rs[:, :, 1])
 
-    return (lon, lat, plot_var)
+    return convert_arguments(P, lon, lat, plot_var)
 
 end
-
-output_series[Ti(1)][:Θᵤ]
-convert_arguments(output_series[Ti(1)][:Θᵤ])
 
 heatmap(output_series[Ti(1)][:Θᵤ])
 
