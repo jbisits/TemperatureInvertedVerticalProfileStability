4e7e19ea0f7fbaddb90f032266a28917d5fa336a
jbisits <jbisits@gmail.com>
2024-02-19 20:03:12 +1100

    Update paper_plots.jl

--- scripts/paper_plots.jl
+++ scripts/paper_plots.jl
@@ -398,7 +398,6 @@ density_grad = get(ColorSchemes.dense, range(0.25, 1, length = 3))
 ΔΘ_thres_vals = (0.5, 1.0, 2.0)
 fig = Figure(size = (500, 500))
 xtickposition = -2:0
-p_ref = 0
 ax = Axis(fig[1, 1];
           title = "Initial static density difference at model interface",
           xlabel = "Initial ΔΘ (°C) at model interface",
